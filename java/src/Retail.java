4df5e05400bcb83bffc1a6cdb88f70d7e0bcc01e
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-11-19 16:26:32 -0800

    Added this.userID to keep track of current user

--- java/src/Retail.java
+++ java/src/Retail.java
@@ -34,6 +34,7 @@ public class Retail {
 
    // reference to physical database connection.
    private Connection _connection = null;
+   private int userID = 0;
 
    // handling the keyboard inputs through a BufferedReader
    // This variable can be global for convenience.
@@ -386,8 +387,8 @@ public class Retail {
          String password = in.readLine();
 
          String query = String.format("SELECT * FROM USERS WHERE name = '%s' AND password = '%s'", name, password);
-         int userNum = esql.executeQuery(query);
-	 if (userNum > 0)
+         this.useID = esql.executeQuery(query);
+	 if (this.userID > 0)
 		return name;
          return null;
       }catch(Exception e){
