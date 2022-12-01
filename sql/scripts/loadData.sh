35adbf12022e887a794975058bf5e109e30cabfe
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 02:09:53 -0800

    Deleted unnecessary files

--- sql/scripts/loadData.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-cp ./*.dat /tmp/$USER/myDB/data
-psql -h localhost -p 8192 "$USER"_DB < ./Lab_8/create_tables.sql
--- sql/scripts/readFile.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-export PGPORT=8192
-psql -h localhost -p $PGPORT "$USER"_DB < $1
