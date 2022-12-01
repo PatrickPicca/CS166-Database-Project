409e867d93b1bde0fb046f8d26878475c2eabbba
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 01:35:08 -0800

    Updated scripts for universal compatibility

--- sql/scripts/setup.sh
+++ sql/scripts/setup.sh
@@ -2,6 +2,9 @@
 source ./startPostgreSQL.sh
 sleep 2
 source ./createPostgreDB.sh
+dir=$(git rev-parse --show-toplevel)
+sed -i "s+/extra/tlian020/VirtualBox VMs/CS166-Database-Project-Phase-3+$dir+g" ../src/load_data.sql > ../src/load_data.sql.tmp
 source ./create_db.sh
 sleep 2
+rm ../src/load_data.sql.tmp
 psql -h localhost -p $PGPORT $USER"_DB"
