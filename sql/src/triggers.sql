f9dc4e50c88676c9c9dad92137e2e2da1abac5c1
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 00:23:55 -0800

    Added trigger for setting new userID

--- sql/src/triggers.sql
+++ sql/src/triggers.sql
@@ -1,3 +1,4 @@
+--Trigger for order numbers and dates
 DROP SEQUENCE IF EXISTS order_number_seq;
 CREATE SEQUENCE order_number_seq START WITH 500;
 
@@ -19,3 +20,25 @@ BEFORE INSERT
 ON Orders
 FOR EACH ROW
 EXECUTE PROCEDURE set_new_order_info();
+
+--Trigger for new user IDs
+DROP SEQUENCE IF EXISTS user_id_seq;
+CREATE SEQUENCE user_id_seq START WITH 100;
+
+CREATE OR REPLACE LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION set_new_user_id() 
+RETURNS "trigger" AS 
+$BODY$
+BEGIN
+    NEW.userID = nextval('user_id_seq');
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+DROP TRIGGER IF EXISTS user_id_trigger ON Users;
+CREATE TRIGGER user_id_trigger
+BEFORE INSERT
+ON Users
+FOR EACH ROW
+EXECUTE PROCEDURE set_new_user_id();
