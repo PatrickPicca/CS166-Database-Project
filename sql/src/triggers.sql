9c39556d27ad9069f4b15130a20d2f5c73eb82ff
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 00:25:18 -0800

    Fixed sequence starting value issue

--- sql/src/triggers.sql
+++ sql/src/triggers.sql
@@ -1,6 +1,6 @@
 --Trigger for order numbers and dates
 DROP SEQUENCE IF EXISTS order_number_seq;
-CREATE SEQUENCE order_number_seq START WITH 500;
+CREATE SEQUENCE order_number_seq START WITH 501;
 
 CREATE OR REPLACE LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION set_new_order_info() 
@@ -23,7 +23,7 @@ EXECUTE PROCEDURE set_new_order_info();
 
 --Trigger for new user IDs
 DROP SEQUENCE IF EXISTS user_id_seq;
-CREATE SEQUENCE user_id_seq START WITH 100;
+CREATE SEQUENCE user_id_seq START WITH 101;
 
 CREATE OR REPLACE LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION set_new_user_id() 
