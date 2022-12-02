dbf720d4b12cb5b77f58902b590ee988ce49792f
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 17:17:13 -0800

    Fixed a problem with repeatedly creating folders

--- sql/scripts/setup.sh
+++ sql/scripts/setup.sh
@@ -4,7 +4,9 @@ sleep 2
 source ./createPostgreDB.sh
 dir=$(git rev-parse --show-toplevel)
 sed -i "s+/extra/tlian020/VirtualBox VMs/CS166-Database-Project-Phase-3+$dir+g" ../src/load_data.sql
-mkdir "$dir/java/classes"
+if [ ! -d "$dir/java/classes" ]; then
+  mkdir "$dir/java/classes"
+fi
 source ./create_db.sh
 sleep 2
 psql -h localhost -p $PGPORT $USER"_DB"
