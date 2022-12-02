639a9f95b05242b520cc3be9739482b9fe750447
Tangyuan Liang <tlian020@wch133-21.cs.ucr.edu>
2022-12-01 17:13:46 -0800

    Fixed a problem with missing folders

--- sql/scripts/setup.sh
+++ sql/scripts/setup.sh
@@ -4,6 +4,7 @@ sleep 2
 source ./createPostgreDB.sh
 dir=$(git rev-parse --show-toplevel)
 sed -i "s+/extra/tlian020/VirtualBox VMs/CS166-Database-Project-Phase-3+$dir+g" ../src/load_data.sql
+mkdir "$dir/java/classes"
 source ./create_db.sh
 sleep 2
 psql -h localhost -p $PGPORT $USER"_DB"
