ecf99ff8d0c0e77c8753fe6fb10a5748f778b598
William Picca <wpicc001@wch135-37.cs.ucr.edu>
2022-12-05 10:54:57 -0800

    Fixed problem with DB server

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -2,7 +2,7 @@
 export PGPORT=8192
 export PGDATA=$folder/myDB/data
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -h localhost -p $PGPORT $USER"_DB" < ../src/create_tables.sql
 psql -h localhost -p $PGPORT $USER"_DB" < ../src/create_indexes.sql
 psql -h localhost -p $PGPORT $USER"_DB" < ../src/load_data.sql
